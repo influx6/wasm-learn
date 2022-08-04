b5c23b72ba62d651d011549b46f74be0cb6c73b4
Alexander Ewetumo <trinoxf@gmail.com>
2022-08-04 20:47:49 +0800

    UPT: Add bindgenhello

---
+++ bindgenhello/dist/bindgenhello.d.ts
@@ -0,0 +1,6 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} name
+*/
+export function hello(name: string): void;
---
+++ bindgenhello/dist/bindgenhello.js
@@ -0,0 +1,2 @@
+import * as wasm from "./bindgenhello_bg.wasm";
+export * from "./bindgenhello_bg.js";
