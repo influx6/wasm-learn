aed2e31c77f93edcaad47dbfe536095d74445e76
Alexander Ewetumo <trinoxf@gmail.com>
2022-08-07 21:13:10 +0800

    UPT: Add wasmi-checkers

--- wasmi-checkers/src/main.rs
+++ wasmi-checkers/src/main.rs
@@ -27,5 +27,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     let board_display = game.get_board_contents()?;
     println!("game board after 1 move:\n{}\n", board_display);
 
+    /*
+        So remember the key points:
+        1. Implement a module ImportResolver implementing the wasmi-> ModuleImportResolver.resolve_func (returns a signature and index via FuncRef to the wasm module)
+        2. Implement a runtime that takes a index which it uses to execute the target function with the args list it receives (it invokes these via implementation of wasmi::Externals.invoke_index)
+        3. Instantiate your wasm module (assgining the moduleimport resolver) and on every function invocation pass in our runtime.
+     */
+
     Ok(())
 }
